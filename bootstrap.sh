bdcbf6c6cd8f49f5651e49ab1eaea445bc1a53eb
Tomasz Giereś <tomasz.gieres@gmail.com>
2013-06-14 23:32:06 +0200

    Better bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 # tmux
-echo "\n### Creating symlinks for tmux..."
-ln -sf $(pwd)/.tmux.conf ~/.tmux.conf && echo "Created for .tmux.conf"
+echo "# Creating symlinks for tmux..."
+ln -sf $(pwd)/.tmux.conf ~/.tmux.conf && echo "[x] .tmux.conf"
 
 # vim
-echo "\n### Creating symlinks for vim..."
-ln -sf $(pwd)/.vimrc ~/.vimrc && echo "Created for .vimrc"
-ln -sf $(pwd)/.vim ~/.vim && echo "Created for .vim/"
+echo
+echo "# Creating symlinks for vim..."
+ln -sf $(pwd)/.vimrc ~/.vimrc && echo "[x] .vimrc"
+# ln -sf $(pwd)/.vim ~/.vim && echo "Created for .vim/"
+
+# vundle
+echo
+echo "### Is vundle installed?"
+VUNDLE_DIR="~/.vim/bundle/vundle"
+if [ -d "$VUNDLE_DIR" ]; then
+  echo "$VUNDLE_DIR exist, skipping..."
+else
+  echo "$VUNDLE_DIR doesn't exist, downloading..."
+  mkdir -p "$VUNDLE_DIR"
+  git clone https://github.com/gmarik/vundle.git "$VUNDLE_DIR" && echo "[x] vundle"
+fi
